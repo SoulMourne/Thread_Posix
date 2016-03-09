70137927868532fb8611b5aad984e1cd9ac387f2
etude <etude@dinfo196.dinfo.iut-velizy.uvsq.fr>
2016-03-09 15:11:03 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: etude
+ *
+ * Created on 9 mars 2016, 14:41
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct thread_args
+{
+    int toto;
+    float titi;
+    char hello[256];
+};
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    struct thread_args a;
+    
+    return (EXIT_SUCCESS);
+}
+
