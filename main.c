de5e81fc2eb426191da8015fad6a7e68ba645052
etude <etude@dinfo113.dinfo.iut-velizy.uvsq.fr>
2016-03-15 16:52:56 +0100

    Fin exercice 1

--- main.c
+++ main.c
@@ -16,21 +16,15 @@
 #include <pthread.h>
 
 void* helloWorld();
+void exercice1();
 
 /*
  * 
  */
 int main(int argc, char** argv) 
 {
-    //Creation de 2 threads
-    
-    pthread_t a;
-    pthread_create(&a, NULL, helloWorld, (void*)pthread_self());
-    pthread_t b;
-    pthread_create(&b, NULL, helloWorld, (void*)pthread_self());
-    pthread_join(a,NULL);
-    
-    printf("%lu\n",pthread_self());
+    printf("--------- exercice 1 -----------\n");
+    exercice1();
     
     return (EXIT_SUCCESS);
 }
@@ -40,3 +34,16 @@ void* helloWorld(void* args)
     printf("Hello from the %lu side !\n", (long unsigned int)args);
 }
 
+void exercice1()
+{
+        //Creation de 2 threads
+    
+    pthread_t a;
+    pthread_create(&a, NULL, helloWorld, (void*)pthread_self());
+    pthread_t b;
+    pthread_create(&b, NULL, helloWorld, (void*)pthread_self());
+    pthread_join(a,NULL);
+    pthread_join(b,NULL);
+    
+    printf("%lu\n",pthread_self());
+}
